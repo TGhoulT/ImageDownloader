4922c732ee70718e6c77053881baedb00cb58128
TGhoulT <mgenius012975@gmail.com>
2024-05-03 11:51:30 +0300

    Main changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,71 +1,46 @@
 package org.example;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.FileOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.HashSet;
 import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String url = "https://skillbox.ru/";
-        Document doc = Jsoup.connect(url).get();
+        try {
+            ImageUrlExtractor imageUrlExtractor = new ImageUrlExtractor(url);
+            Set<String> imageLinks = imageUrlExtractor.getImageUrls();
 
-        Set<String> imageLinks = new HashSet<>();
-
-        /*System.out.println(doc);*/    //проверяем, получили ли мы исходный код сайта
-
-        Elements elements = doc.select("img");
-
-        /*elements.forEach(System.out::println);*/    //просмотр картинок в исходном коде с тегом img
-
-        for (Element image : elements) { //получаем все элементы с тегом img и добавляем ссылки на эти изобр. в хэшсет
-            String imageUrl = image.attr("abs:src");
-            imageLinks.add(imageUrl);
-        }
-
-        int number = 1;
-
-        for (String link : imageLinks) {
-            try {
-                String extension = link.replaceAll("^.+\\.", "").replace("?.+$", "");
-
-                //формируем путь для сохранения файла
-                String filePath = "images/" + number++ + "." + extension;
-
-                //скачиваем файл и сохраняем его по указанному пути
-                downloadImage(link, filePath);
-            } catch (IOException e) {
-                //пропускаем итерацию цикла при возникновении исключения (кириллица в ссылке)
-                System.out.println("Ошибка при скачивании изображения: " + e.getMessage());
+            ImageDownloader imageDownloader = new ImageDownloader();
+            createImagesFolder(); // Создание папки "images", если ее нет
+            int number = 1;
+            for (String link : imageLinks) {
+                try {
+                    String extension = link.replaceAll("^.+\\.", "").replace("?.+$", "");
+                    String filePath = "images/" + number++ + "." + extension;
+                    imageDownloader.downloadImage(link, filePath);
+                } catch (IOException e) {
+                    System.out.println("Ошибка при скачивании изображения: " + e.getMessage());
+                }
             }
+
+            System.out.println("Ссылки на изображения:");
+            for (String link : imageLinks) {
+                System.out.println(link);
+            }
+        } catch (IOException e) {
+            e.getMessage();
         }
-
-        //все ссылки, которые мы привели к нормальному виду через регулярку
-        System.out.println("Ссылки на изображения:");
-        for (String link : imageLinks) {
-            System.out.println(link);
-        }
-
-
     }
 
-    private static void downloadImage(String link, String filePath) throws IOException {
-        URLConnection connection = new URL(link).openConnection();
-        try (InputStream inStream = connection.getInputStream();
-             OutputStream outStream = new FileOutputStream(filePath)) {
-            byte[] buffer = new byte[4096];
-            int bytesRead;
-            while ((bytesRead = inStream.read(buffer)) != -1) {
-                outStream.write(buffer, 0, bytesRead);
+    //если не существует папки images то создаём её
+    private static void createImagesFolder() {
+        File folder = new File("images");
+        if (!folder.exists()) {
+            if (folder.mkdir()) {
+                System.out.println("Папка 'images' успешно создана.");
+            } else {
+                System.out.println("Не удалось создать папку 'images'.");
             }
         }
     }
